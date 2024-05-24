cd59d2a3fab03dfda8138a888f7f7271586a75be
Strange <rivasjoaquin02@gmail.com>
2024-05-24 19:26:33 -0400

    build: add shadcn.ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
     return (
         <h1 className="text-3xl font-bold underline">
+            <Button> Click me </Button>
             Hello world!
         </h1>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './globals.css'
+import DotPattern from './components/ui/dot-pattern.tsx'
+import { cn } from './lib/utils.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
+        {/* <DotPattern
+            width={20}
+            height={20}
+            cx={1}
+            cy={1}
+            cr={1}
+            className={cn(
+                "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] ",
+            )}
+        /> */}
         <App />
     </React.StrictMode>,
 )
